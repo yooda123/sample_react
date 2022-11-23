99fcf29141cdff247e1f9952af98117be2c2d7ac
yooda123 <y0o0y321@gmail.com>
2022-11-23 18:41:35 +0900

    05_state_and_event_020_useState

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,32 +1,15 @@
-import "./Example.css";
+import { useState } from 'react'
 
 const Example = () => {
+  let [val, setVal] = useState(0);
+  // console.log(valArray);
   return (
-    <div>
-      <h3>コンソールを確認してください。</h3>
-      <label>
-        入力値のイベント：
-        <input
-          type="text"
-          onChange={() => console.log("onChange検知")}
-          onBlur={() => console.log("onBlur検知")}
-          onFocus={() => console.log("onFocus検知")}
-        />
-      </label>
-      <div>
-        <label>
-          入力値を取得：
-          <input type="text" onChange={(e) => console.log(e.target.value)} />
-        </label>
-      </div>
-      <div
-        className="hover-event"
-        onMouseEnter={() => console.log("カーソルが入ってきました。")}
-        onMouseLeave={() => console.log("カーソルが出ていきました。")}
-      >
-        ホバーしてね！
-      </div>
-    </div>
+    <>
+      <input 
+        type="text"
+        onChange={(e) => setVal(e.target.value)}
+      /> = {val} 
+    </>
   );
 };
 
