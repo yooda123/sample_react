8a5a8dea83777a17f3f166b2002950c3abe48382
yooda123 <y0o0y321@gmail.com>
2022-11-23 22:59:42 +0900

    05_state_and_event_055_practice_state

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,17 +1,19 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Example = () => {
-  const [valA, setValA] = useState(0);
-  const [valB, setValB] = useState(0);
-  const [valC, setValC] = useState(0);
+  const [ count, setCount ] = useState(0);
+  const countUp = ()=>{
+    // setCount(count+1);
+    setCount(prevstate => {
+      console.log(prevstate+1);
+      return prevstate + 1;
+    })
+  }
   return (
     <>
-      <p>ボタンAを{valA}回押しました！</p>    
-      <button onClick = {() => {setValA(valA+1);}}>ボタンA</button>
-      <p>ボタンBを{valB}回押しました！</p>    
-      <button onClick = {() => {setValB(valB+1);}}>ボタンB</button>
-      <p>ボタンCを{valC}回押しました！</p>    
-      <button onClick = {() => {setValC(valC+1);}}>ボタンC</button>
+      <p>現在のカウント数：{count}</p>
+      <button onClick={countUp}>+</button>
+      <button onClick={()=>{setCount(count-1)}}>-</button>
     </>
   )
 };
