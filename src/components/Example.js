e7ffb0cf20f6935c7644692ffd24d5e0087a5f6e
yooda123 <y0o0y321@gmail.com>
2022-11-24 00:48:13 +0900

    05_state_and_event_060_state_object

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,21 +1,31 @@
 import { useState } from "react";
 
 const Example = () => {
-  const [ count, setCount ] = useState(0);
-  const countUp = ()=>{
-    // setCount(count+1);
-    setCount(prevstate => {
-      console.log(prevstate+1);
-      return prevstate + 1;
-    })
+  const personObj = { name: "Tom", age: 18 };
+  const [person, setPerson] = useState(personObj)
+
+  const changeName = (e) => {
+      // setPerson({ name: e.target.value, age: person.age });
+      setPerson({ ...person, name: e.target.value });
   }
+  const changeAge = (e) => {
+    // setPerson({ name: person.name, age: e.target.value });
+    setPerson({ ...person, age: e.target.value });
+  }
+  const reset = () => {
+    setPerson({ name: "", age: "" });
+  }
+
   return (
     <>
-      <p>現在のカウント数：{count}</p>
-      <button onClick={countUp}>+</button>
-      <button onClick={()=>{setCount(count-1)}}>-</button>
+      <h3>Name: {person.name}</h3>
+      <h3>Age: {person.age}</h3>
+      <div><input type="text" onChange={changeName} value={person.name} /></div>
+      <div><input type="number" onChange={changeAge} value={person.age} /></div>
+      <button onClick={reset}>リセット</button>
     </>
   )
+
 };
 
 export default Example;
