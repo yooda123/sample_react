f8b853185f8bc4248ae8069a69a6731971415741
yooda123 <y0o0y321@gmail.com>
2022-11-23 13:37:41 +0900

    075_fragment

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,11 +1,17 @@
-import { List } from "./List"
+import React from "react"
 
 const Child = () => {
   return (
+    <React.Fragment key="">
     <div className="component">
       <h3>Hello Example Component</h3>
-      <List />
     </div>
+    <h3>Hello Fragment</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+        Voluptatibus amet recusandae quod illo dolore explicabo soluta beatae, 
+        iure ullam minima debitis quisquam excepturi perferendis aliquid aut 
+        quos odio pariatur perspiciatis!</p>
+    </React.Fragment>
   )
 }
 
