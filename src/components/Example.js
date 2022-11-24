eb487c7e8203dee423b14664d185a85b482cb5a6
yooda123 <y0o0y321@gmail.com>
2022-11-24 23:20:50 +0900

    05_state_and_event_068_practice_obj_state

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,31 +1,25 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  const personObj = { name: "Tom", age: 18 };
-  const [person, setPerson] = useState(personObj)
-
-  const changeName = (e) => {
-      // setPerson({ name: e.target.value, age: person.age });
-      setPerson({ ...person, name: e.target.value });
-  }
-  const changeAge = (e) => {
-    // setPerson({ name: person.name, age: e.target.value });
-    setPerson({ ...person, age: e.target.value });
-  }
-  const reset = () => {
-    setPerson({ name: "", age: "" });
-  }
-
+  const orderObj = { item: 'apple', count: 10 };
+  const [order, setOrder] = useState(orderObj);
+  // const changeItem = (e) => {setOrder({...order, item: e.target.value})};
+  const changeItem = (e) => {setOrder(order => ({...order, item: e.target.value}))};
+  const countUp = (e) => {setOrder({...order, count: order.count+1})};
+  const countDown = (e) => {setOrder({...order, count: order.count-1})};
   return (
-    <>
-      <h3>Name: {person.name}</h3>
-      <h3>Age: {person.age}</h3>
-      <div><input type="text" onChange={changeName} value={person.name} /></div>
-      <div><input type="number" onChange={changeAge} value={person.age} /></div>
-      <button onClick={reset}>リセット</button>
-    </>
-  )
-
+    <div>
+      <h3>練習問題</h3>
+      <p>
+        記述を変更し、完成コードのように「+と-ボタンをクリックするとCountの表示が1ずつ増減する機能」と「input要素に連動してItemの表示が変更される機能」を実装してください。コンポーネントの外側（上部）に変数や関数を準備しているためうまく使ってください。
+      </p>
+      <h3>Item:{order.item}</h3>
+      <h3>Count:{order.count}</h3>
+      <input type="text" value={order.item} onChange={changeItem} />
+      <button onClick={countUp}>+</button>
+      <button onClick={countDown}>-</button>
+    </div>
+  );
 };
 
 export default Example;
