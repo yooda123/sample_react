f0bf58a2b9c1b609a7181e07dcbbe70c42223fb8
yooda123 <y0o0y321@gmail.com>
2022-11-25 07:15:54 +0900

    05_state_and_event_080_state_and_props

--- src/components/Example.js
+++ src/components/Example.js
@@ -3,19 +3,23 @@ import { useState } from "react";
 
 const Example = () => {
   const [toggle, setToggle] = useState(true)
+  const [countA, setCountA] = useState(0);
+  const [countB, setCountB] = useState(0);
   const toggleComponent = () => {
     setToggle(!toggle);
   }
   return (
     <>
       <button onClick={toggleComponent}>Toggle</button>
-      {toggle ? <Count key="A" title="A"/> : <Count key="B" title="B"/>}
+      {toggle
+      ? <Count key="A" title="A" count={countA} setCount={setCountA} /> 
+      : <Count key="B" title="B" count={countB} setCount={setCountB}/>
+      }
     </>
   )
 }
 
-const Count = ({title}) => {
-  const [count, setCount] = useState(0);
+const Count = ({title, count, setCount}) => {
   const countUp = () => {
     setCount((prevstate) => prevstate + 1);
   };
