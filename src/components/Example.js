454d376338bd9b5702b482e05a295e5b96e52b15
yooda123 <y0o0y321@gmail.com>
2022-11-24 23:37:45 +0900

    05_state_and_event_064_state_array

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,24 +1,19 @@
-import { useState } from 'react';
+import { useState } from 'react'
 
 const Example = () => {
-  const orderObj = { item: 'apple', count: 10 };
-  const [order, setOrder] = useState(orderObj);
-  // const changeItem = (e) => {setOrder({...order, item: e.target.value})};
-  const changeItem = (e) => {setOrder(order => ({...order, item: e.target.value}))};
-  const countUp = (e) => {setOrder({...order, count: order.count+1})};
-  const countDown = (e) => {setOrder({...order, count: order.count-1})};
+  const numArray = [1, 2, 3, 4, 5];
+  const [nums, setNums] = useState(numArray);
+  const shuffle = () => {
+    const newNums = [...nums];
+    const i = newNums.pop();
+    newNums.unshift(i);
+    setNums(newNums);
+  }
   return (
-    <div>
-      <h3>練習問題</h3>
-      <p>
-        記述を変更し、完成コードのように「+と-ボタンをクリックするとCountの表示が1ずつ増減する機能」と「input要素に連動してItemの表示が変更される機能」を実装してください。コンポーネントの外側（上部）に変数や関数を準備しているためうまく使ってください。
-      </p>
-      <h3>Item:{order.item}</h3>
-      <h3>Count:{order.count}</h3>
-      <input type="text" value={order.item} onChange={changeItem} />
-      <button onClick={countUp}>+</button>
-      <button onClick={countDown}>-</button>
-    </div>
+    <>
+      <h1>{nums}</h1>
+      <button onClick={shuffle}>shuffle</button>
+    </>
   );
 };
 
