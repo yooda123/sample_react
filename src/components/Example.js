1b0fef86691d9d044fc9e583db208f3111b0ac1c
yooda123 <y0o0y321@gmail.com>
2022-11-26 10:49:16 +0900

    done!

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,63 +1,27 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const Example = () => {
-  const [fruits, setFruits] = useState([
-    { label: "Apple", value: 100, checked: false },
-    { label: "Banana", value: 200, checked: false },
-    { label: "Cherry", value: 300, checked: false },
-  ]);
-
-  const [sum, setSum] = useState(0);
-
-  const handleChange = (e) => {
-    const newFruits = fruits.map((fruit) => {
-      const newFruit = fruit;
-      if (newFruit.label === e.target.value) {
-        newFruit.checked =  !newFruit.checked;
-      }
-      return newFruit;
-    });
-    setFruits(newFruits);
-    
-    let sumVal = 0;
-    // newFruits.forEach(fruit => {
-    //   if (fruit.checked) {
-    //     sumVal += fruit.value; 
-    //   }
-    // });
-
-    // newFruits
-    //   .filter(fruit => fruit.checked)
-    //   .forEach(fruit => sumVal += fruit.value); 
-
-    sumVal = newFruits
-      .filter((fruit) => fruit.checked)
-      .reduce((sumVal, fruit) => sumVal += fruit.value, 0)
-    setSum(sumVal);
-  }
+  const [selected, setSelected] = useState("Banana");
+  const OPTIONS = ['Apple', 'Banana', "Cherry"];
+  const handerChange = (e) => {
+    setSelected(e.target.value);
+  };
 
   return (
-    <div>
-      {fruits.map((fruit) => {
-        return (
-          // <>
-          <label key={fruit.label} htmlFor={fruit.label}>
-          <input
-            id={fruit.label}
-            type="checkbox"
-            value={fruit.label}
-            checked={fruit.checked}
-            onChange={handleChange} 
-          />
-          {fruit.label}:{fruit.value}
-          </label>
-          // </>
-        )
-      }
-      )}
-      <div>合計：{sum}</div>
-    </div>
-  );
+    <>
+    <select
+      value={selected}
+      onChange={handerChange}
+    >
+      {OPTIONS.map((item) => {
+        return <option value={item}>{item}</option>        
+      })};
+    </select>
+    <div>選択された果物: {selected}</div>
+
+    </>
+    );
+    
 };
 
 export default Example;
