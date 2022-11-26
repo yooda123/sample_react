955f1c96108976c702409164f887de44b5ce8762
yooda123 <y0o0y321@gmail.com>
2022-11-26 10:12:03 +0900

    done!

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,37 +1,62 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 
 const Example = () => {
-  const [isChecked, setIsChecked] = useState(false);
-  const toggleChecked = (val) => {
-    console.log(val);
-    setIsChecked((prev) => {
-      console.log(prev);
-      return !prev;
+  const [fruits, setFruits] = useState([
+    { label: "Apple", value: 100, checked: false },
+    { label: "Banana", value: 200, checked: false },
+    { label: "Cherry", value: 300, checked: false },
+  ]);
+
+  const [sum, setSum] = useState(0);
+
+  const handleChange = (e) => {
+    const newFruits = fruits.map((fruit) => {
+      const newFruit = fruit;
+      if (newFruit.label === e.target.value) {
+        newFruit.checked =  !newFruit.checked;
+      }
+      return newFruit;
     });
-  } 
-  // const toggleChecked = (e) => {
-  //   setIsChecked((prevState) => {
-  //     console.log(prevState);
-  //     let state = !prevState;
-  //     return state;
-  //   });
-  // } 
+    setFruits(newFruits);
+    
+    let sumVal = 0;
+    // newFruits.forEach(fruit => {
+    //   if (fruit.checked) {
+    //     sumVal += fruit.value; 
+    //   }
+    // });
+
+    // newFruits
+    //   .filter(fruit => fruit.checked)
+    //   .forEach(fruit => sumVal += fruit.value); 
+
+    sumVal = newFruits
+      .filter((fruit) => fruit.checked)
+      .reduce((sumVal, fruit) => sumVal += fruit.value, 0)
+    setSum(sumVal);
+  }
 
   return (
-    <>
-      <label htmlFor="my-check">
-        チェック
-      </label>
-      <input 
-        type="checkbox"
-        id="my-check"
-        checked={isChecked}
-        onChange={(e) => toggleChecked(e)}
-        // onChange={(e) => toggleChecked(e)}
-      />
-      <h3>{isChecked ? "チェックON" : "チェックOFF"}</h3>
-    </>
+    <div>
+      {fruits.map((fruit) => {
+        return (
+          // <>
+          <label key={fruit.label} htmlFor={fruit.label}>
+          <input
+            id={fruit.label}
+            type="checkbox"
+            value={fruit.label}
+            checked={fruit.checked}
+            onChange={handleChange} 
+          />
+          {fruit.label}:{fruit.value}
+          </label>
+          // </>
+        )
+      }
+      )}
+      <div>合計：{sum}</div>
+    </div>
   );
 };
 
