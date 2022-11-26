78369e1d9772039d94a12df44a79b935658a977c
yooda123 <y0o0y321@gmail.com>
2022-11-27 08:30:19 +0900

    done!

--- src/Example.js
+++ src/Example.js
@@ -1,54 +1,13 @@
-import { useState } from "react";
-import styled from 'styled-components';
+import { ChakraProvider } from "@chakra-ui/react";
 
-// POINT 拡張機能 styled-components.vscode-styled-components
-
-console.dir(styled);
-const StyledButton = styled.button`
-  margin: auto;
-  border-radius: 9999px;
-  border: none;
-  display: block;
-  width: 120px;
-  height: 60px;
-  font-weight: bold;
-  cursor: pointer;
-  background: ${({isSelected}) => isSelected ? 'pink' : ''};
-
-  @media (max-width: 600px) {
-    border-radius: 0;
-  }
-`;
-
-const OrangeButton = styled(StyledButton)`
-  background-color: orange;
-  :hover, :active {
-    color: red;
-    opacity: 0.7;
-  }
-
-  span {
-    font-size: 2em; 
-  }
-
-`;
-
+import Todo from "./components/Todo"
 
 const Example = () => {
-  const [isSelected, setIsSelected] = useState(false);
-
-  const clickHandler = () => setIsSelected((prev) => !prev);
-
   return (
     <>
-      <StyledButton isSelected={isSelected} onClick={clickHandler}>ボタンです</StyledButton>
-      <OrangeButton><span>押してね</span></OrangeButton>
-      <button className={`btn ${isSelected ? "selected" : ""}`} onClick={clickHandler}>
-        ボタン
-      </button>
-      <div style={{ textAlign: "center" }}>
-        {isSelected && "クリックされました。"}
-      </div>
+      <ChakraProvider>
+      <Todo />
+      </ChakraProvider>
     </>
   );
 };
