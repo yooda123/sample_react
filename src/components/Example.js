f64022b3b56cbcbfec9805d4314a7d94608110c9
yooda123 <y0o0y321@gmail.com>
2022-11-26 16:03:41 +0900

    done!

--- src/components/Example.js
+++ src/components/Example.js
@@ -1,11 +1,30 @@
-import Todo from "./Todo";
+import { useState } from "react"
 
 const Example = () => {
-  return (
-    <>
-      <Todo />
-    </>
-  );
+    const [isSelected, setIsSelected] = useState(false);
+
+    const clickHandler = () => setIsSelected(prev => !prev);
+
+    const style = {
+      width: 120,
+      height: 60,
+      display: 'block',
+      fontWeight: "bold",
+      // "font-weight": "bold",
+      "border-radius": 9999,
+      // borderRadius: 9999,
+      cursor: "pointer",
+      border: "none",
+      margin: "auto",
+      background: isSelected ? "pink" : "",
+    }
+
+    return (
+        <>
+            <button onClick={clickHandler} style={style}>ボタン</button>
+            <div style={{ textAlign: "center" }}>{isSelected && "クリックされました。"}</div>
+        </>
+    )
 };
 
 export default Example;
