0dff638385e4898073ee89b814a1bb5c7e876ab2
yooda123 <y0o0y321@gmail.com>
2022-11-27 18:19:19 +0900

    done!

--- src/Example.js
+++ src/Example.js
@@ -1,24 +1,9 @@
-import { useState } from "react"
-
-const Child = ({count}) => {
-  return <p>現在のカウント数: {count.val}</p>
-}
+import Todo from './components/Todo';
 
 const Example = () => {
-  const [count, setCount] = useState({ val: 0 });
-
-  const countUp = () => {
-    debugger
-    setCount((prevstate) => {
-      const newState = { val: prevstate.val + 1 }
-      return newState;
-    });
-  };
-
   return (
     <>
-      <Child count={count} />
-      <button onClick={countUp}>+</button>
+      <Todo />
     </>
   );
 };
