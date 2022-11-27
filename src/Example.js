cf486c2337e1cf50740777a185c9a12716597b3d
yooda123 <y0o0y321@gmail.com>
2022-11-27 09:18:03 +0900

    focused textbox with useRef.

--- src/Example.js
+++ src/Example.js
@@ -1,41 +1,27 @@
-import { useState } from "react";
-import Toast from "./components/Toast";
-import { createPortal } from "react-dom";
+import { useRef, useState } from "react";
 
+const Case1 = () => {
+  const [value, setValue] = useState("");
+  const inputRef = useRef();
 
-const Example = () => {
-  const [toastOpen, setToastOpen] = useState(false);
+  // console.log(inputRef);
 
-  const ModalPortal = ({ children }) => {
-    const target = document.querySelector(".container.start");
-    return createPortal(children, target);
-  }
-  
   return (
     <div>
-      <h3>
-        トーストの作成（createPortal）
-      </h3>
-      <p>ボタンを押すと.container.start要素にマウントされて、表示されるトーストを作成してください。トーストにはToastコンポーネントを使用してください。</p>
-
-      <div className="container start"></div>
-      
-      <button
-        type="button"
-        onClick={() => setToastOpen(true)}
-        disabled={toastOpen}
-      >
-        トーストを表示する
+      <h3>ユースケース1</h3>
+      <input type="text" ref={inputRef} value={value} onChange={(e) => setValue(e.target.value)} />
+      <button onClick={() => inputRef.current.focus()}>
+        インプット要素をフォーカスする
       </button>
-      {toastOpen && (
-        <ModalPortal>
-          <Toast
-            visible={toastOpen}
-            handleCloseClick={() => setToastOpen(false)}
-          />
-        </ModalPortal>
-      )}
     </div>
+  );
+};
+
+const Example = () => {
+  return (
+    <>
+      <Case1 />
+    </>
   );
 };
 
