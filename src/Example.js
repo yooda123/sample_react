2bdc9f66128f67e754132bb911cf3afd0177bcf5
yooda123 <y0o0y321@gmail.com>
2022-11-27 18:37:06 +0900

    done!

--- src/Example.js
+++ src/Example.js
@@ -1,21 +1,23 @@
+// 引数で渡ってきたオブジェクトを変更しない！
+
 const Example = () => {
-  // 関数型（純粋関数）
-  // POINT fn(決まった引数) -> 決まった戻り値
-  // POINT 関数外の状態（データ）は参照・変更しない。
-  // POINT 関数外に影響を及ぼさない。
-  // ・引数で渡された値を変更しない。
-  // 不変性()
-  // 上記の要件を満たさない操作は「副作用」と呼ぶ。
-  
-  const val1 = 2, val2 =  30;
-  const add = (val1, val2) => {
-    return val1 + val2;
+  const num = { val: 10 };
+  const double = (num) => {
+    // num.val =  num.val * 2;
+    // return num;
+    const newNum = {...num, val: num.val * 2}
+    return newNum;
   }
+
+  const newNum = double(num);
+  console.log('newNum: ' , newNum, 'num: ', num);
+
+  console.log(newNum === num);
 
   return (
     <>
-      <h3>純粋関数</h3>
-      <p>{add(val1, val2)}</p>
+      <h3>不変性（Immutability）</h3>
+      
     </>
   );
 };
