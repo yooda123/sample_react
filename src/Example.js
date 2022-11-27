478bd9efa12fa657fcbd93aa8fd46f63daeef114
yooda123 <y0o0y321@gmail.com>
2022-11-27 10:13:41 +0900

    done!

--- src/Example.js
+++ src/Example.js
@@ -17,10 +17,60 @@ const Case1 = () => {
   );
 };
 
+const Case2 = () => {
+  const [playing, setPlaying] = useState(false);
+  const videoRef = useRef();
+
+  return (
+    <>
+      <h3>ユースケース2</h3>
+      <video style={{ maxWidth: "80%" }} ref={videoRef}>
+        <source src="./sample.mp4"></source>
+      </video>
+      <button onClick={() => {
+        playing ? videoRef.current.pause() : videoRef.current.play();
+        setPlaying(!playing);
+      }}>
+        { playing ? 'Stop' : 'Start'}
+      </button>
+    </>
+  )
+}
+
+const Case3 = () => {
+  const createTimestamp = () => new Date().getTime();
+  const [timestamp, setTimestamp] = useState(createTimestamp());
+  const ref = useRef(createTimestamp());
+
+  const updateState = () => {
+    setTimestamp(createTimestamp());
+  }
+  const updateRef = () => {
+    ref.current = createTimestamp();
+    console.log("ref.current --> ", ref.current);
+  }
+
+  return (
+    <>
+      <h3>ユースケース3</h3>
+      <p>
+        state: {timestamp}
+        <button onClick={updateState}>更新</button>
+      </p>
+      <p>
+        ref: {ref.current}
+        <button onClick={updateRef}>更新</button>
+      </p>
+    </>
+  )
+}
+
 const Example = () => {
   return (
     <>
       <Case1 />
+      <Case2 />
+      <Case3 />
     </>
   );
 };
