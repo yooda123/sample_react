35c0ab454ee6019c5da9f084981724ca37926660
yooda123 <y0o0y321@gmail.com>
2022-12-02 18:53:28 +0900

    done

--- src/Example.js
+++ src/Example.js
@@ -2,80 +2,75 @@ import { useReducer } from "react";
 
 const CALC_OPTIONS = ["add", "minus", "divide", "multiply"];
 
-const reducer = (state, {type, payload}) => {
-  switch (type) {
-    case "change":
-        const ret = {...state, [payload.name]: payload.value}
-        // debugger
-        return ret;
-    case "add":
-      return {...state, result: state.a + state.b};
-    case "minus":
-      return {...state, result: state.a - state.b};
-    case "divide":
-      return {...state, result: state.a / state.b};
-    case "multiply":
-      return {...state, result: state.a * state.b};
+const reducer = (state, { payload }) => {
+  const { name, value } = payload;
+  const newState = { ...state, [name]: value };
+ 
+  switch (newState.type) {
+    case "add": {
+      return {
+        ...newState,
+        result: parseInt(newState.a) + parseInt(newState.b),
+      };
+    }
+    case "minus": {
+      return { ...newState, result: newState.a - newState.b };
+    }
+    case "divide": {
+      return { ...newState, result: newState.a / newState.b };
+    }
+    case "multiply": {
+      return { ...newState, result: newState.a * newState.b };
+    }
     default:
-      return new Error('不正な演算子です');
+      throw new Error("operator is invalid");
   }
-}
-
+};
+ 
 const Example = () => {
   const initState = {
     a: 1,
     b: 2,
     result: 3,
+    type: "add",
   };
-
+ 
   const [state, dispatch] = useReducer(reducer, initState);
-
-  const calculate = (e) => {
-    dispatch(
-      { type: e.target.value }
-    );
+ 
+  const inputChangeHandler = (e) => {
+    dispatch({
+      payload: { name: e.target.name, value: e.target.value },
+    });
   };
-
-  const numChangeHandler = (e) => {
-    dispatch(
-      { type: 'change', payload: { name: e.target.name, value: parseInt(e.target.value) } }
-    );
-  }
-
+ 
   return (
     <>
-    <h3>練習問題</h3>
-    <p>useReducerを使って完成コードと同じ機能を作成してください。</p>
-    <div>
-    a:
-    <input
-      type="number"
-      name="a"
-      value={state.a}
-      onChange={numChangeHandler}
-    />
-    </div>
-    <div>
-    b:
-    <input
-      type="number"
-      name="b"
-      value={state.b}
-      onChange={numChangeHandler}
-      // onChange={() => {
-      //   numChangeHandler();
-      //   // calculate();
-      // }}
-    />
-    </div>
-    <select onChange={calculate}>
-      {CALC_OPTIONS.map((item) => {
-        return (
-         <option key={item} value={item}>{item}</option>
-        )  
-      })}
-    </select>
-    <h3>結果：{state.result}</h3>
+      <div>
+        a:
+        <input
+          type="number"
+          name="a"
+          value={state.a}
+          onChange={inputChangeHandler}
+        />
+      </div>
+      <div>
+        b:
+        <input
+          type="number"
+          name="b"
+          value={state.b}
+          onChange={inputChangeHandler}
+        />
+      </div>
+      <select value={state.type} name="type" onChange={inputChangeHandler}>
+        {CALC_OPTIONS.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+      <h3>結果：{state.result}</h3>
     </>
   );
 };
