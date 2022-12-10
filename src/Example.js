5d04560a94c9a4551696fd77f92cd14465ca0039
yooda123 <y0o0y321@gmail.com>
2022-12-10 12:17:01 +0900

    init

--- src/Example.js
+++ src/Example.js
@@ -1,18 +1,16 @@
-import Input from "./components/Input";
-import Select from "./components/Select";
-import Result from "./components/Result";
-import { CalcProvider } from "./context/CalcContext";
+import Todo from "./components/Todo"
 
 const Example = () => {
-
   return (
     <>
-      <CalcProvider>
-        <Input name="a" />
-        <Input name="b" />
-        <Select />
-        <Result />
-      </CalcProvider>
+    <h3>練習問題</h3>
+    <ul>
+      <li>Listコンポーネント内の各項目をItemコンポーネントに分離しましょう。</li>
+      <li>タイトルをダブルクリックするとタイトルを変更出来るようにしましょう</li>
+      <li>ReducerとContextを使ってTodoをグローバルなステートにしましょう</li>
+    </ul>
+      <h2>Reminder</h2>
+      <Todo />
     </>
   );
 };
